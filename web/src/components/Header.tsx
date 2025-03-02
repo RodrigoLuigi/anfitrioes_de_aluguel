8c1b4f1e56604b856b567deab7277b47d871f3f7
Rodrigo Luigi <digaor@gmail.com>
2025-03-02 02:37:25 -0300

    style: adjust in social icons size

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -3,7 +3,7 @@ import anfitrioesLogo from "../assets/anfitrioes_de_aluguel_logo.png";
 import vooTrajetoria from "../assets/voo-trajetoria.png";
 export function Header() {
 	return (
-		<header className="flex flex-col items-center gap-2 bg-white p-4 border-b border-solid border-indigo-950 lg:flex-row lg:p-0 lg:px-24 lg:justify-between">
+		<header className="flex flex-col items-center gap-2 bg-white p-4 border-b border-solid border-indigo-950 relative overflow-hidden sm:flex-row lg:p-0 lg:px-24 sm:justify-between">
 			<img
 				src={anfitrioesLogo}
 				alt="logo anfitriões de aluguel"
@@ -13,40 +13,40 @@ export function Header() {
 			<img
 				src={vooTrajetoria}
 				alt="trajetória voo de avião"
-				className="w-96 h-28"
+				className="w-96 h-28 absolute top-12 -rotate-[25deg] opacity-25 sm:relative sm:rotate-0 sm:top-0 sm:opacity-100"
 			/>
 
 			<div className="flex flex-col gap-2">
-				<span className="text-blue-900 font-bold text-xs lg:text-base">
+				<span className="text-blue-900 font-normal text-xs lg:text-sm">
 					Developed by: Rodrigo Luigi
 				</span>
 
 				<div className="flex justify-center gap-4">
 					<a
 						href="https://www.instagram.com/lrodrigo_luigi/"
-						className="size-8 lg:size-12 bg-blue-900 rounded-md flex items-center justify-center text-[#ffffff] outline-none hover:text-blue-300 focus:ring-blue-500 focus:ring-2"
+						className="size-6 lg:size-10 bg-blue-900 rounded-md flex items-center justify-center text-[#ffffff] outline-none hover:text-blue-300 focus:ring-blue-500 focus:ring-2"
 						target="_blank"
 						rel="noreferrer"
 					>
-						<Instagram className="size-5 lg:size-8" />
+						<Instagram className="size-3 lg:size-6" />
 					</a>
 
 					<a
 						href="https://www.linkedin.com/in/rodrigo-luigi/"
-						className="size-8 lg:size-12 bg-blue-900 rounded-md flex items-center justify-center text-[#ffffff] outline-none hover:text-blue-300 focus:ring-blue-500 focus:ring-2"
+						className="size-6 lg:size-10 bg-blue-900 rounded-md flex items-center justify-center text-[#ffffff] outline-none hover:text-blue-300 focus:ring-blue-500 focus:ring-2"
 						target="_blank"
 						rel="noreferrer"
 					>
-						<Linkedin className="size-5 lg:size-8" />
+						<Linkedin className="size-3 lg:size-6" />
 					</a>
 
 					<a
 						href="https://github.com/RodrigoLuigi"
-						className="size-8 lg:size-12 bg-blue-900 rounded-md flex items-center justify-center text-[#ffffff] outline-none hover:text-blue-400 focus:ring-blue-400 focus:ring-2"
+						className="size-6 lg:size-10 bg-blue-900 rounded-md flex items-center justify-center text-[#ffffff] outline-none hover:text-blue-400 focus:ring-blue-400 focus:ring-2"
 						target="_blank"
 						rel="noreferrer"
 					>
-						<Github className="size-5 lg:size-8" />
+						<Github className="size-3 lg:size-6" />
 					</a>
 				</div>
 			</div>
