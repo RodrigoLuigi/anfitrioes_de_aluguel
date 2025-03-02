88fb57cca780b65a84b255ae788772f4ac8ed126
Rodrigo Luigi <digaor@gmail.com>
2025-03-02 13:27:44 -0300

    feat: create handleFavorite an implements localstorage

--- web/src/pages/Home/components/card-accommodation.tsx
+++ web/src/pages/Home/components/card-accommodation.tsx
@@ -1,4 +1,5 @@
-import { MapPin, Star } from "lucide-react";
+import { useState, useEffect } from "react";
+import { Heart, MapPin, Star } from "lucide-react";
 import type { AccommodationProps } from "..";
 
 interface CardAccommodationProps {
@@ -6,6 +7,31 @@ interface CardAccommodationProps {
 }
 
 export function CardAccommodation({ accommodation }: CardAccommodationProps) {
+	const [isFavorited, setIsFavorited] = useState(false);
+
+	useEffect(() => {
+		const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+		setIsFavorited(favorites.includes(accommodation.id));
+	}, [accommodation.id]);
+
+	function handleFavorite(event: React.MouseEvent<HTMLButtonElement>) {
+		event.preventDefault();
+
+		const favorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+
+		if (isFavorited) {
+			const updatedFavorites = favorites.filter(
+				(id: string) => id !== accommodation.id,
+			);
+			localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+			setIsFavorited(false);
+		} else {
+			favorites.push(accommodation.id);
+			localStorage.setItem("favorites", JSON.stringify(favorites));
+			setIsFavorited(true);
+		}
+	}
+
 	return (
 		<a
 			href={`/details/${accommodation.id}`}
@@ -17,9 +43,22 @@ export function CardAccommodation({ accommodation }: CardAccommodationProps) {
 				className="shadow-lg shadow-zinc-500/50 rounded-t-md w-full h-[200px]"
 			/>
 
-			<div className="p-6 flex flex-col gap-9">
+			<div className="p-6 flex flex-col gap-9 relative">
+				<button
+					type="button"
+					className="absolute right-4 top-4 cursor-pointer z-10"
+					onClick={handleFavorite}
+				>
+					<Heart
+						className={`size-6 transition-all duration-300 ${
+							isFavorited
+								? "fill-red-500 text-red-500"
+								: "text-gray-400 hover:text-red-500"
+						}`}
+					/>
+				</button>
 				<div className="flex flex-col items-center justify-center gap-2">
-					<h2 className="text-xl font-bold text-blue-900">
+					<h2 className="text-xl font-bold text-blue-900 hover:bg-blue-700">
 						{accommodation.name}
 					</h2>
 					<div className="flex items-center gap-1">
@@ -31,7 +70,7 @@ export function CardAccommodation({ accommodation }: CardAccommodationProps) {
 					</div>
 				</div>
 
-				<div className="flex gap-1 items-center justify-between  text-blue-500">
+				<div className="flex gap-1 items-center justify-between text-blue-500">
 					<span className="font-bold text-2xl">
 						R$
 						{Number(accommodation.price_per_night).toFixed(2).replace(".", ",")}
