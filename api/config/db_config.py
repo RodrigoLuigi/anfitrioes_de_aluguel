b592671276e1bb7bd0af9a44dd66e9878dcb8d7c
Rodrigo Luigi <digaor@gmail.com>
2025-03-02 00:21:26 -0300

    add: dotenv config

--- api/config/db_config.py
+++ api/config/db_config.py
@@ -1,5 +1,9 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:123456@localhost:5432/acomodacoes')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
